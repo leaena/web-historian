d5e0707e7a6a6f6bcd751e9f0ed306af4f3cf169
Lindsey Smith <lindseyengineer@gmail.com>
2013-12-09 15:03:23 -0800

    first push

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 
-exports.headers = headers = {
+var headers = headers = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
   "access-control-allow-headers": "content-type, accept",
@@ -9,9 +9,27 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+var extensions = {
+  '.html': "text/html",
+  '.js': "text/javascript",
+  '.css': "text/css",
+  '.ico': "image/ico"
+}
+
 exports.serveStaticAssets = function(res, folder, asset) {
   //Write some code here that helps serve up your static files!
   //(Static files are things like html (yours or arhived from others...), css, or anything that doesn't change often.)
+  fs.readFile(folder + asset, function (err, html) {
+    if (err) {
+      throw err;
+    }
+    headers['Content-type'] = extensions[path.extname(asset)];
+    res.writeHeader(200, headers);
+    res.write(html);
+    res.end();
+    });
 };
+
+exports.headers;
 
 // As you go through, keep thinking about what helper functions you can put here!
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,19 @@
 var path = require('path');
+var url = require('url');
+var helpers = require("./http-helpers");
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
+var requests = {
+  'GET': helpers.serveStaticAssets
+}
+
+var paths = {
+  '/': 'index.html'
+}
+
 module.exports.handleRequest = function (req, res) {
-  console.log(exports.datadir);
+  var pathname = url.parse(req.url).pathname;
+  pathname = paths[pathname] || pathname;
+  var method = requests[req.method];
+  method(res, 'web/public/', pathname);
 };
